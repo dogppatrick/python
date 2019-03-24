246858a2ea86c670968a1297674b91cab5863988
dogppatrick@msn.com <dogppatrick@msn.com>
2019-03-24 14:09:31 +0800

    add database api test

--- function_api_test/test_mongodb.py
+++ function_api_test/test_mongodb.py
@@ -1,5 +1,7 @@
 from pymongo import MongoClient
-import datetime
+from bson.objectid import ObjectId
+# pymongo tutirial https://api.mongodb.com/python/current/tutorial.html
+# _id need import bson.objectid
 # MongoClient(host='localhost', port=27017, document_class=dict,
 #             tz_aware=False, connect=True, **kwargs)
 # client = MongoClient('mongodb://localhost:27017/')
@@ -10,9 +12,9 @@ db = client["test0324"]
 c = db.c1
 # _id not define >> _id auto create
 new_fruit = {"fruit": ["mongo", "melon"],
-             "size": 7}
+             "size": 9}
 # insert
-c.insert_one(new_fruit)
+# c.insert_one(new_fruit)
 # c = client["test0324"].c1
 # c.find()  = db.c1.find in mongodb
 for data in c.find():
@@ -20,8 +22,8 @@ for data in c.find():
     # for fruit in fruits:
     #     print(fruit, end="\t")
     print(data)
-    # print("")
-
-print(c.find_one())
+print("===")
+# querying by find({})
+print(c.find_one({"_id": ObjectId('5c971e256e5cc32b50e95b02')}))
 # new_fruit
 
