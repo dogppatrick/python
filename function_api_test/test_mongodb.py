5439e9de0873a61a4b86c73cdc1d66bc584d1e68
dogppatrick@msn.com <dogppatrick@msn.com>
2019-03-24 13:58:56 +0800

    add database api test

--- function_api_test/test_mongodb.py
+++ function_api_test/test_mongodb.py
@@ -1,14 +1,26 @@
 from pymongo import MongoClient
+import datetime
+# MongoClient(host='localhost', port=27017, document_class=dict,
+#             tz_aware=False, connect=True, **kwargs)
 # client = MongoClient('mongodb://localhost:27017/')
-client = MongoClient("localhost", 27017)
-# select db
-db = client["iii-2019-03"]
-# select collection
-c = db.food
-# c = client["iii-2019-03"].food
-# c.find()  = db.c.find in mongodb
+client = MongoClient()
+# select db create if no db
+db = client["test0324"]
+# select collection create in no collection
+c = db.c1
+# _id not define >> _id auto create
+new_fruit = {"fruit": ["mongo", "melon"],
+             "size": 5}
+c.insert_one(new_fruit)
+# c = client["test0324"].c1
+# c.find()  = db.c1.find in mongodb
 for data in c.find():
     fruits = data["fruit"]
-    for fruit in fruits:
-        print(fruit, end="\t")
+    # for fruit in fruits:
+    #     print(fruit, end="\t")
+    print(data)
     print()
+# insert
+
+# new_fruit
+
